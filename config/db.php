1cc724456e819c9380a56cee6fc259ea1c5a4043
Nosthertus <darkfreak17@hotmail.com>
2015-11-10 15:11:23 -0430

    Commit inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=hl',
+    'username' => 'root',
+    'password' => '3141',
+    'charset' => 'utf8',
+];
