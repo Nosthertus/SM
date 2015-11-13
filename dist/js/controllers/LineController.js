055ab57f25e82a473bf61b10f84bce7d2b3898d3
Nosthertus <darkfreak17@hotmail.com>
2015-11-13 13:33:45 -0430

    Reparacion de packery en "line" y sistema de rutas nuevo
    *Se reparo la funcion que no activaba el framework en el links de "line" (la funcion esta en la vista, buscar si se puede correr la misma funcion en un archivo js)
    *Se agrego un nuevo sistema de rutas que verifica agrega links en el navbar dependiendo de los valores en el archivo MainController.js
    *Se agrego un evento que redirecciona a root cuando la ruta escrita no esta registrada

--- dist/js/controllers/LineController.js
+++ dist/js/controllers/LineController.js
@@ -4,26 +4,3 @@
 	app.controller('LineController', ['UserService', '$location', '$scope', function(UserService, $location, $scope){
 	}]);
 })(angular);
-
-// external js: packery.pkgd.js
-
-var notifElem;
-
-$( function() {
-  notifElem = $('#notification')[0];
-  
-  var $container = $('.packery').packery();
-  
-  $container.on( 'layoutComplete', function( event, laidOutItems ) {
-    console.log( 'Packery layout completed on ' + laidOutItems.length + ' items' );
-  });
-
-  $container.on( 'click', '.item', function( event ) {
-    // change size of item via class
-    $( event.target ).toggleClass('gigante');
-    // trigger layout
-    $container.packery();
-  });
-
-
-});
