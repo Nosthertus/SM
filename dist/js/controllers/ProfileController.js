3542418f726ff821bc951f7f0304e3750c5d5a75
magdiel <magdielikari@gmail.com>
2015-11-18 02:56:04 -0800

    Cambios menores de la vista y actividades de form asincronicos pendientes en list y network...

--- dist/js/controllers/ProfileController.js
+++ dist/js/controllers/ProfileController.js
@@ -1,7 +1,7 @@
 (function(angular){
 	var app = angular.module('SM');
-
 	app.controller('ProfileController', ['$scope', '$location', 'UserService', function($scope, $location, UserService){
+		$scope.name=UserService.getName();
 		$scope.success = function(file, message, flow){
 			console.log(JSON.parse(message));
 		};
