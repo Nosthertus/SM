26547709056082b7d8c4555be36894368a330d12
Nosthertus <darkfreak17@hotmail.com>
2015-11-18 00:26:21 -0430

    Modificacion de packery.js
    *Ahora el javascript esta dentro de un archivo js
    *Se agrego packery en list y profile

---
+++ dist/js/layout.js
@@ -0,0 +1,22 @@
+$(function(){
+	var $container = $('.packery').packery();
+
+	$container.on('layoutComplete', function(event, laidOutItems){
+		console.log( 'Packery layout completed on ' + laidOutItems.length + ' items' );
+	});
+
+	$container.on( 'click', '.item', function(event){
+		if($(event.target).parent().hasClass('z1')){
+			$(event.target).parent().toggleClass('a1');
+		}
+
+		if($(event.target).parent().hasClass('z2')){
+			$(event.target).parent().toggleClass('a2');
+		}
+
+		if($(event.target).parent().hasClass('z3')){
+			$(event.target).parent().toggleClass('a3');
+		}
+		$container.packery();
+	});
+});
